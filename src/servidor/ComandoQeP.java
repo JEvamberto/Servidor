00692932254f7376785ea2e8af5f5462c5e274fb
José Evamberto <evamberto.lima@gmail.com>
2018-08-25 23:59:17 -0300

    Comando  P e Q

--- src/servidor/ComandoQeP.java
+++ src/servidor/ComandoQeP.java
@@ -5,23 +5,72 @@
  */
 package servidor;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author jose
  */
 public class ComandoQeP implements Runnable {
 
-    public ComandoQeP (){
+    
+    private EnviarUDP[] estacao;
+    private Thread thread;
+    private ArrayList<Socket> clientes;
+    
+    public ComandoQeP (EnviarUDP[] estacao, ArrayList <Socket> cliente ){
+        this.estacao=estacao;
+        this.clientes=cliente;
     
     }
     
     @Override
     public void run() {
-       
+        
+       Scanner teclado;
+       char comando;
         while (true){
+            teclado= new Scanner (System.in);
+            System.out.println("Comando P ou Q:");
+            comando=teclado.nextLine().charAt(0);
+            
+            if (comando=='P' || comando== 'p') {
+                this.listarClientes();
+            }else if(comando == 'Q' || comando == 'q'){
+                this.fecharConexao();
+            }
+            teclado=null;
+            
+            
         
         }
         
+    }
+    
+    public void listarClientes(){
+        System.out.println("Cliente no servidor"+clientes.size());
+        
+        for (int i = 0; i < this.estacao.length; i++) {
+            
+            System.out.println("Estação: "+i+" Clientes: "+estacao[i].getQuantidadeDeOuvintes() );
+        }
+    }
+    
+    public void fecharConexao(){
+        
+        for (Socket cliente : clientes) {
+            
+            try {
+                cliente.close();
+            } catch (IOException ex) {
+                Logger.getLogger(ComandoQeP.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
     
 }
